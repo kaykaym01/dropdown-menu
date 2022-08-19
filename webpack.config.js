2c345cc055b02ee06ec5b53e020f0577a1db8c5a
kaykaym01 <kaymoore01@gmail.com>
2022-08-19 08:59:35 -0400

    Add css and style loader to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,31 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
-  entry: './src/js/index.js',
-  devtool: 'inline-source-map',
+  entry: "./src/js/index.js",
+  devtool: "inline-source-map",
   plugins: [
     new HtmlWebpackPlugin({
-        title: 'Output Management',
+      title: "Output Management",
     }),
   ],
   output: {
-    filename: 'index.bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "index.bundle.js",
+    path: path.resolve(__dirname, "dist"),
     clean: true,
   },
   devServer: {
-    static: path.resolve(__dirname, 'dist'),
+    static: path.resolve(__dirname, "dist"),
     port: 8080,
-    hot: true
-  }
-}
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
