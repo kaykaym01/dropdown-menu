ef1ab10515f84dfc53973137a6dda42888b47118
kaykaym01 <kaymoore01@gmail.com>
2022-08-19 08:41:35 -0400

    Add htmlwebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
-const path = require('path')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/js/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+        title: 'Output Management',
+    }),
+  ],
   output: {
     filename: 'index.bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   devServer: {
     static: path.resolve(__dirname, 'dist'),
