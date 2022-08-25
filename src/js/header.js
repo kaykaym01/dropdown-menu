b0139988bd32a5758ca0f2b2e9757c525dbe33ac
kaykaym01 <kaymoore01@gmail.com>
2022-08-24 23:53:53 -0400

    Add down arrow to header

--- src/js/header.js
+++ src/js/header.js
@@ -3,8 +3,13 @@ function header(){
     header.classList.add("header");
 
     const title = document.createElement("h1");
-    title.textContent = "Dropdown Menu";
+    title.textContent = "Dropdown Menu ";
     header.appendChild(title);
+
+    const dropdownButton = document.createElement("button");
+    dropdownButton.classList.add("dropdown-btn")
+    dropdownButton.textContent +=  String.fromCharCode(0x25BE);
+    header.append(dropdownButton);
 
     return header;
 }
--- src/js/main.js
+++ src/js/main.js
@@ -5,4 +5,6 @@ function main(){
     return main;
 }
 
+
+
 export default main;
