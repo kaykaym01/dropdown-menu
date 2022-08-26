00f9bb7b8909b501185392125ee11b3baeeed164
kaykaym01 <kaymoore01@gmail.com>
2022-08-25 23:48:31 -0400

    Add onclick event for dropdown button to show menu

--- src/js/header.js
+++ src/js/header.js
@@ -1,34 +1,49 @@
-function header(){
-    const header = document.createElement("header");
-    header.classList.add("header");
+function header() {
+  const header = document.createElement("header");
+  header.classList.add("header");
 
-    const title = document.createElement("h1");
-    title.textContent = "Dropdown Menu ";
-    header.appendChild(title);
+  const title = document.createElement("h1");
+  title.textContent = "Dropdown Menu ";
+  header.appendChild(title);
 
-    const dropdownButton = document.createElement("button");
-    dropdownButton.classList.add("dropdown-btn")
-    dropdownButton.textContent +=  String.fromCharCode(0x25BE);
-    header.append(dropdownButton);
+  header.appendChild(dropdown());
 
-    header.append(menu());
-
-    return header;
+  return header;
 }
 
-function menu(){
-    const list = document.createElement("ul");
-    list.classList.add("menu");
-    
-    const listItems = ["Pricing", "Demo", "Features"];
-    listItems.forEach((value) => {
-        const listItem = document.createElement("li");
-        listItem.classList.add("menu-item");
-        listItem.textContent = value;
-        list.appendChild(listItem);
-    });
+function dropdown() {
+  const dropdown = document.createElement("div");
+  dropdown.classList.add("dropdown");
 
-    return list;
+  const dropdownButton = document.createElement("button");
+  dropdownButton.classList.add("dropdown-btn");
+  dropdownButton.textContent += String.fromCharCode(0x25be);
+  dropdownButton.addEventListener("click", toggleDropdown);
+  dropdown.append(dropdownButton);
+
+  dropdown.append(menu());
+
+  return dropdown;
+}
+
+function toggleDropdown() {
+  const dropdownMenu = document.querySelector(".menu");
+  dropdownMenu.classList.toggle("show");
+}
+
+function menu() {
+  const list = document.createElement("ul");
+  list.classList.add("menu");
+
+  const listItems = ["Pricing", "Demo", "Features"];
+  listItems.forEach((value) => {
+    const listItem = document.createElement("li");
+    listItem.classList.add("menu-item");
+    listItem.textContent = value;
+    list.appendChild(listItem);
+  });
+
+  return list;
 }
 
 export default header;
